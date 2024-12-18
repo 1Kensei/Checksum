c0b37c91aff6113e136ccc9421c616f7f9c4013d
Dias <149446708+1Kensei@users.noreply.github.com>
2024-12-18 05:01:36 +0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+stty -echo
+
+PATHWAY="$(pwd)"
+ESCPATHWAY="$(pwd | sed "s/\//\\\\\//g")"
+
+ARRAY="$(find "${PATHWAY}" -type f -exec md5sum {} \; | sed "s/$ESCPATHWAY/./g")"
+
+for entry in "${ARRAY}"
+do
+  printf "$entry\n"
+done
