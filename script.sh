dd56a37384f1d2901381da8102313b5c720efed6
Dias <149446708+1Kensei@users.noreply.github.com>
2024-12-21 21:03:15 +0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,10 +3,9 @@ stty -echo
 
 PATHWAY="$(pwd)"
 ESCPATHWAY="$(pwd | sed "s/\//\\\\\//g")"
-
-ARRAY="$(find "${PATHWAY}" -type f -exec md5sum {} \; | sed "s/$ESCPATHWAY/./g")"
+ARRAY="$(find "${PATHWAY}" -type f -exec md5sum {} \; | sed "s/$ESCPATHWAY/.\//g")"
 
 for entry in "${ARRAY}"
 do
-  printf "$entry\n"
+    printf "${entry}\n"
 done
